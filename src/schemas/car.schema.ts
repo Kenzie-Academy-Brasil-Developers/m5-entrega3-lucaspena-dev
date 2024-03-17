9a8190f0c2b6209acbe447be66efa4c9ab8d5a8e
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-16 22:07:35 -0300

    refactor(schema.prisma): removed desnecessary comment in the file

---
+++ src/schemas/car.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const carSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string().nullish(),
+  brand: z.string(),
+  year: z.number().positive(),
+  km: z.number().positive(),
+});
+
+const carCreateSchema = carSchema.omit({ id: true });
+const carUpdateSchema = carCreateSchema.partial();
+const carReturnSchema = carSchema;
+
+export { carSchema, carCreateSchema, carUpdateSchema, carReturnSchema };
---
+++ src/schemas/index.ts
@@ -0,0 +1,8 @@
+import {
+  carSchema,
+  carCreateSchema,
+  carUpdateSchema,
+  carReturnSchema,
+} from "./car.schema";
+
+export { carSchema, carCreateSchema, carUpdateSchema, carReturnSchema };
