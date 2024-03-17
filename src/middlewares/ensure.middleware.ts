17f7343d056ee76d875034ae7ae582cf0077a6c0
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-17 16:39:21 -0300

    feat: added EnsureMiddleware with two methods called validateBody and isCarIdValid

---
+++ src/middlewares/ensure.middleware.ts
@@ -0,0 +1,32 @@
+import { container, injectable } from "tsyringe";
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+
+import { prisma } from "../database/prisma";
+import { AppError } from "../errors";
+
+@injectable()
+class EnsureMiddleware {
+  validateBody(schema: AnyZodObject) {
+    return async (
+      { body }: Request,
+      _res: Response,
+      next: NextFunction
+    ): Promise<void> => {
+      body = await schema.parseAsync(body);
+
+      return next();
+    };
+  }
+
+  isCarIdValid({ params }: Request, _res: Response, next: NextFunction) {
+    const { id } = params;
+    const car = prisma.car.findUnique({ where: { id } });
+
+    if (!car) throw new AppError(404, "Car not found.");
+
+    return next();
+  }
+}
+
+export const ensure = container.resolve(EnsureMiddleware);
