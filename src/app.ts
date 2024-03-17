b91b568cbb1203c4f6c5c621ea87db2fd7576211
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-16 23:00:14 -0300

    feat(app.ts): implement the router (carRouter) and the route /cars

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import "express-async-errors";
 import express, { Application, json } from "express";
 import helmet from "helmet";
 
+import { carRouter } from "./routers";
+
 export const app: Application = express();
 
 app.use(helmet());
 app.use(json());
+
+app.use("/cars", carRouter);
