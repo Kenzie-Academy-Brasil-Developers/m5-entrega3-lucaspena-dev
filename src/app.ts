b4de4615c8c5b5eb96125ab12d3106f9c99415d4
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-16 21:28:13 -0300

    feat(app.ts | server.ts): added initial setup to start the application

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import "express-async-errors";
+import express, { Application, json } from "express";
+import helmet from "helmet";
+
+export const app: Application = express();
+
+app.use(helmet());
+app.use(json());
