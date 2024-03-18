feea367e9f2dc4c8c6188982ae9e5a61c551559a
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-17 21:07:07 -0300

    test: added unit test for the create method in CarServices.ts

---
+++ src/__tests__/units/services/car/create.test.ts
@@ -0,0 +1,22 @@
+import { CarServices } from "../../../../services";
+import { prismaMock } from "../../../__mocks__/prisma";
+import { carMock } from "../../../__mocks__/units/services/car";
+import { carDefaultSpecs } from "../../../utils";
+
+describe("Unit tests: create car", () => {
+  const { body, expectedValue } = carMock;
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Should be able to create a new car correctly", async () => {
+    prismaMock.car.create.mockResolvedValue(expectedValue);
+
+    const carServices = new CarServices().create(body);
+    const received = await carServices;
+
+    expect(received.id).toBeDefined();
+    carDefaultSpecs(received, expectedValue);
+  });
+});
