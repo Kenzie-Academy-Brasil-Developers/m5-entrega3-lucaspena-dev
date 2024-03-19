9e86cf435b2f79ca531cde1b2d3506b10954a6f5
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-19 18:29:04 -0300

    test(services): validate error handling for invalid car creation

--- src/__tests__/units/services/car/create.test.ts
+++ src/__tests__/units/services/car/create.test.ts
@@ -1,10 +1,12 @@
 import { Car } from "@prisma/client";
+import { PrismaClientValidationError } from "@prisma/client/runtime/library";
+
 import { carMock } from "../../../__mocks__/units/services";
 import { prisma } from "../../../../database/prisma";
 import { CarServices } from "../../../../services";
 import { carDefaultSpecs } from "../../../utils";
 
-describe("Unit test: Create car service", () => {
+describe("Unit tests: Create car service", () => {
   const { body, expectedValue } = carMock;
   const carTb = prisma.car;
 
@@ -21,5 +23,13 @@ describe("Unit test: Create car service", () => {
     const received = (await carServices(body)) as Car;
 
     carDefaultSpecs(received, expectedValue);
+  });
+
+  test("Should not be able to create a car with invalid data", async () => {
+    const carServices = new CarServices().create;
+
+    expect(async () => {
+      await carServices({} as any);
+    }).rejects.toThrow(PrismaClientValidationError);
   });
 });
