99cdf361ba6a38e94b00a686316a64478d0dbec4
Lucas Pena <134110552+lucaspena-dev@users.noreply.github.com>
2024-03-19 16:47:06 -0300

    test(data): add car mock data for tests

---
+++ src/__tests__/__mocks__/units/services/car.mock.ts
@@ -0,0 +1,101 @@
+const carMock = {
+  body: {
+    name: "Carro 1",
+    brand: "Brand 1",
+    year: 2020,
+    km: 0,
+  },
+  expectedValue: {
+    id: expect.any(String),
+    name: "Carro 1",
+    description: null,
+    brand: "Brand 1",
+    year: 2020,
+    km: 0,
+  },
+};
+
+const carMockList = {
+  body: [
+    {
+      name: "Carro 1",
+      brand: "Brand 1",
+      year: 2020,
+      km: 0,
+    },
+    {
+      name: "Carro 2",
+      description: "Description 2",
+      brand: "Brand 2",
+      year: 2019,
+      km: 0,
+    },
+    {
+      name: "Carro 3",
+      description: "Description 3",
+      brand: "Brand 3",
+      year: 2022,
+      km: 0,
+    },
+    {
+      name: "Carro 4",
+      brand: "Brand 4",
+      year: 2023,
+      km: 0,
+    },
+  ],
+  expectedValue: [
+    {
+      id: expect.any(String),
+      name: "Carro 1",
+      description: null,
+      brand: "Brand 1",
+      year: 2020,
+      km: 0,
+    },
+    {
+      id: expect.any(String),
+      name: "Carro 2",
+      description: "Description 2",
+      brand: "Brand 2",
+      year: 2021,
+      km: 0,
+    },
+    {
+      id: expect.any(String),
+      name: "Carro 3",
+      description: "Description 3",
+      brand: "Brand 3",
+      year: 2022,
+      km: 0,
+    },
+    {
+      id: expect.any(String),
+      name: "Carro 4",
+      description: null,
+      brand: "Brand 4",
+      year: 2023,
+      km: 0,
+    },
+  ],
+};
+
+const carUpdateMock = {
+  body: {
+    name: "Carro 1",
+    brand: "Brand 1",
+    description: "Description 1",
+    year: 2020,
+    km: 0,
+  },
+  expectedValue: {
+    id: expect.any(String),
+    name: "Carro 1",
+    description: "Description 1",
+    brand: "Brand 1",
+    year: 2019,
+    km: 0,
+  },
+};
+
+export { carMock, carMockList, carUpdateMock };
---
+++ src/__tests__/__mocks__/units/services/index.ts
@@ -0,0 +1,3 @@
+import { carMock, carMockList, carUpdateMock } from "./car.mock";
+
+export { carMock, carMockList, carUpdateMock };
